dd0f599e68cb8394ecd4435d63b08a43c3df7bd4
CornyElephant <eldraco@mac.com>
2015-12-02 12:42:42 -0500

    Testing

--- src/project1/Tower.java
+++ src/project1/Tower.java
@@ -2,28 +2,107 @@ package project1;
 
 public class Tower implements TowerInterface{
 
+	private AscendinglyOrderedListADT<Plane, String> planes;
+	private ListInterface<Plane> waiting;
+	private ListInterface<Runway> runways;
+	private int currentRunway;
+	
+	public Tower()
+	{
+		planes = new ABAscendinglyOrderedList<Plane, String>();
+		runways = new ListArrayBasedPlus<Runway>();
+		waiting = new ListArrayBasedPlus<Plane>();
+		currentRunway = -1;
+	}
+	
 	@Override
 	public boolean isValidRunwayName(String name) {
 		// TODO Auto-generated method stub
+		/*
+		 * sequential search here
+		 */
+		boolean found = false;
+		int index = 0;
+		while(!found && index < runways.size())
+		{
+			if(runways.get(index).getName().equals(name)))
+				
+		}
 		return false;
 	}
 
 	@Override
 	public void addRunway(String name) {
 		// TODO Auto-generated method stub
-		
+		if(isValidRunwayName(name))
+		{
+			runways.add(runways.size(), new Runway(false, true, name));
+		}
 	}
 
-	@Override
-	public boolean isValidFlightNumber(String flightNumber) {
+	/**
+	 * We might want to have tower implement this functionality inside of addPlaneToSystem method for efficiency
+	 */
+	protected int findPlane(String flightNumber) {
 		// TODO Auto-generated method stub
-		return false;
+		int low = 0;
+		int high = planes.size() - 1;
+		int mid = 0;
+		while(low < high)
+		{
+			mid = (low + high)/2;
+			if(planes.get(mid).getKey().compareTo(flightNumber) > 0)
+			{
+				low = mid + 1;
+			}
+			else
+			{
+				high = mid;
+			}
+		}
+		if(planes.get(low).getKey().compareTo(flightNumber) == 0)
+			return low;
+		else
+			return -1;
+	}
+	
+	protected int findRunway(String name)
+	{
+				/*
+				 * sequential search here
+				 */
+				boolean found = false;
+				int index = 0;
+				while(!found && index < runways.size())
+				{
+					if(runways.get(index).getName().equals(name))
+						found = true;
+					else
+							index++;
+						
+				}
+				return index;
 	}
 
 	@Override
 	public void addPlaneToSystem(String flightNumber, String destination,
 			String runwayName) {
-		// TODO Auto-generated method stub
+		int low = 0;
+		int high = planes.size() - 1;
+		int mid = 0;
+		while(low < high)
+		{
+			mid = (low + high)/2;
+			if(planes.get(mid).getKey().compareTo(flightNumber) > 0)
+			{
+				low = mid + 1;
+			}
+			else
+			{
+				high = mid;
+			}
+		}
+		if(planes.get(low).getKey().compareTo(flightNumber) == 0)
 		
 	}
 
